24718946d38caefeddc09d934574697c71bf7045
Teo230 <m.f.tim@live.it>
2024-06-28 17:12:36 +0200

    fix client

--- opcua-client.js
+++ opcua-client.js
@@ -9,48 +9,12 @@ module.exports = function (RED) {
         maxRetry: 100
     };
 
-    let node = this;
-
-    //#region Methods
-
-    function setupClient() {
-        console.debug('Setup opc client for ' + node.name);
-        node.client = opcua.OPCUAClient.create({
-            applicationName: node.name,
-            keepSessionAlive: true,
-            keepAliveInterval: 1000,
-            connectionStrategy: node.connectionStrategy,
-            securityMode: opcua.MessageSecurityMode.None, // TODO
-            securityPolicy: opcua.SecurityPolicy.None, //TODO
-            endpointMustExist: false
-        });
-    }
-
-    async function connect() {
-        await node.client.connect(node.host);
-
-        await createSession();
-    }
-
-    async function createSession() {
-        node.session = await node.client.createSession();
-    }
-
-    function onClientConnected() {
-        // node.warn('Connected');
-        console.warn('Connected');
-    }
-
-    function onClientDisconnected() {
-        // node.warn('Disconnected');
-        console.warn('Disconnected');
-    }
-
-    //#endregion
-
+    // constructor
     function opcUaClientNode(args) {
 
         RED.nodes.createNode(this, args);
+
+        let node = this;
 
         node.name = args.name; //unique name identifier
         node.host = args.host; //opc.tcp
@@ -60,10 +24,44 @@ module.exports = function (RED) {
 
         node.client.on('connected', onClientConnected);
         node.client.on('close', onClientDisconnected);
-        node.client.on('')
+        // node.client.on('')
 
         // Connect client
         connect();
+
+        //#region Methods
+
+        function setupClient() {
+            console.debug('Setup opc client for ' + node.name);
+            node.client = opcua.OPCUAClient.create({
+                applicationName: node.name,
+                keepSessionAlive: true,
+                keepAliveInterval: 1000,
+                connectionStrategy: node.connectionStrategy,
+                securityMode: opcua.MessageSecurityMode.None, // TODO
+                securityPolicy: opcua.SecurityPolicy.None, //TODO
+                endpointMustExist: false
+            });
+        }
+
+        async function connect() {
+            await node.client.connect(node.host);
+            await createSession();
+        }
+
+        async function createSession() {
+            node.session = await node.client.createSession();
+        }
+
+        function onClientConnected() {
+            node.debug('Connected');
+        }
+
+        function onClientDisconnected() {
+            node.debug('Disconnected');
+        }
+
+        //#endregion
     }
 
     RED.nodes.registerType("opcua-client", opcUaClientNode);
