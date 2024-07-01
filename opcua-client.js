bbb5a743193f20be7e886384f4f49c2cc8011dde
Teo230 <m.f.tim@live.it>
2024-07-01 11:20:52 +0200

    show opc status connection

--- opcua-client.js
+++ opcua-client.js
@@ -1,7 +1,11 @@
 module.exports = function (RED) {
 
+    //variables placed here are shared by all nodes
+    var storage = require('node-persist');
     var opcua = require('node-opcua');
 
+    var opcClient = new opcua.OPCUAClient(); // declare here but assign later to mantain type
+    
     // caused the connect method to fail after one single unsuccessful retry
     const connectionStrategy = {
         initialDelay: 1000,
@@ -16,15 +20,21 @@ module.exports = function (RED) {
 
         let node = this;
 
+        //Initialize persist storage
+        storage.init({dir: `${RED.settings.userDir}/opcua-x/persist`});
+
         node.name = args.name; //unique name identifier
         node.host = args.host; //opc.tcp
 
         // Setup client
         setupClient();
 
-        node.client.on('connection_reestablished', onClientReconnected);
-        node.client.on('connected', onClientConnected);
-        node.client.on('close', onClientDisconnected);
+        opcClient.on("abort", () => node.debug("client has aborted"));
+        opcClient.on("close", () => node.debug("client has closed"));
+        opcClient.on("connection_reestablished", () => sendClientConnectionStatus(true));
+        opcClient.on("connection_lost", () => sendClientConnectionStatus(false));
+        opcClient.on("start_reconnection", () => node.debug("client is trying to reconnect"));
+        opcClient.on("after_reconnection", () => node.debug("client start reconnection"));
 
         // Connect client
         connect();
@@ -32,12 +42,12 @@ module.exports = function (RED) {
         //#region Methods
 
         function setupClient() {
-            console.debug('Setup opc client for ' + node.name);
-            node.client = opcua.OPCUAClient.create({
+            node.debug('Setup opc client for ' + node.name);
+            opcClient = opcua.OPCUAClient.create({
                 applicationName: node.name,
                 keepSessionAlive: true,
                 keepAliveInterval: 5000,
-                connectionStrategy: node.connectionStrategy,
+                connectionStrategy: connectionStrategy,
                 securityMode: opcua.MessageSecurityMode.None, // TODO
                 securityPolicy: opcua.SecurityPolicy.None, //TODO
                 endpointMustExist: false
@@ -45,44 +55,31 @@ module.exports = function (RED) {
         }
 
         async function connect() {
-            await node.client.connect(node.host);
+            try {
+                await opcClient.connect(node.host);
+                sendClientConnectionStatus(true);
+            } catch (err) {
+                node.error(err);
+            }
             await createSession();
         }
 
         async function createSession() {
-            node.session = await node.client.createSession();
+            try {
+                node.session = await opcClient.createSession();
+            } catch (err) {
+                node.error(err);
+            }
 
-            node.session.on('keepalive', checkServerStatus);
-            node.session.on('session_closed', onSessionClosed);
-            node.session.on('statusChanged', onSessionStatusChanged);
+            node.session.on("session_closed", () => node.debug("session has closed"));
+            // node.session.on("keepalive", () => node.debug("session keepalive"));
+            // node.session.on("keepalive_failure", () => node.debug("session keepalive failure"));
         }
 
-        function checkServerStatus(status){
-            let state = opcua.EnumServerState[status]
-            node.debug('Server status: ' + state);
-
-            // flow.set("server-status", state); // TO FIX, use context to pass state to opcua-status node
-        }
-
-        function onSessionClosed(){
-            node.debug('Session closed');
-        }
-
-        function onSessionStatusChanged(status){
-            let state = opcua.EnumServerState[status];
-            node.debug('Session status: ' + state);
-        }
-
-        function onClientReconnected(){
-            node.warn('!!! Client Reconnected !!!');
-        }
-
-        function onClientConnected() {
-            node.debug('Connected');
-        }
-
-        function onClientDisconnected() {
-            node.debug('Disconnected');
+        function sendClientConnectionStatus(connected) {
+            if (connected) node.debug("client has reconnected");
+            else node.debug("client has lost connection");
+            storage.setItem("client-connected", connected);
         }
 
         //#endregion
--- opcua-status.js
+++ opcua-status.js
@@ -1,14 +1,30 @@
-module.exports = function(RED) {
+module.exports = function (RED) {
+
+    //variables placed here are shared by all nodes
+    var storage = require('node-persist');
 
     function OpcUaStatusNode(config) {
 
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
+        let node = this;
 
-        //TODO
-        // var state = this.flow.get("server-status");
-        // var msg = {payload: state};
-        // this.send(msg);
+        //Initialize persist storage
+        storage.init({ dir: `${RED.settings.userDir}/opcua-x/persist` });
+
+        setInterval(checkServerConnection, 5000);
+
+        function checkServerConnection() {
+            state = false;
+            if (storage !== null && storage !== undefined) {
+                storage.getItem("client-connected").then((value) =>{
+                    state = value;
+                });
+            }
+            var msg = { payload: state };
+            node.send(msg);
+        }
+
     }
-    RED.nodes.registerType("opcua-status",OpcUaStatusNode);
+    RED.nodes.registerType("opcua-status", OpcUaStatusNode);
 
 }
